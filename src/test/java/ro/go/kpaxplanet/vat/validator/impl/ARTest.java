19fc801af5f0423822326da7064b9b3568b87959
Camilla Serri Colombo <cserri@thoughtworks.com>
2019-07-26 16:06:36 +0200

    add validation for Argentinian vat number

---
+++ src/test/java/ro/go/kpaxplanet/vat/validator/impl/ARTest.java
@@ -0,0 +1,17 @@
+package ro.go.kpaxplanet.vat.validator.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ARTest {
+    @Test
+    public void testValidateAR_11DigitisNumeric() {
+        assertTrue(new AR().validate("12345678901"));
+    }
+
+    @Test
+    public void testNotValidateAR() {
+        assertFalse(new AR().validate("1234567890A"));
+    }
+}
